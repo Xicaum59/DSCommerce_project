1bf1c91b30b80589e228f91f7e5dfa5f57337c99
Xico Utrabo <xico.utrabo@gmail.com>
2025-07-08 21:06:17 -0300

    Many to Many association class 2

--- src/main/java/com/xicaum59/dscommerce/entities/Order.java
+++ src/main/java/com/xicaum59/dscommerce/entities/Order.java
@@ -2,7 +2,9 @@ package com.xicaum59.dscommerce.entities;
 
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -16,90 +18,86 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
-		@Entity
-		@Table(name = "tb_order")
-		public class Order {
+	@Entity
+	@Table(name = "tb_order")
+	public class Order {
 		
-			@Id
-			@GeneratedValue(strategy = GenerationType.IDENTITY)
-			private Long id;
-			
-			@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-			private Instant moment;
-			
-			private OrderStatus status;
+		@Id
+		@GeneratedValue(strategy  = GenerationType.IDENTITY)
+		private Long id;
 		
-			@ManyToOne
-			@JoinColumn(name = "client_id")
-			private User client;
-			
-			@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-			private Payment payment;
-			
-			@OneToMany(mappedBy ="id.order")
-			private Set<OrderItem> items = new HashSet<>();
+		@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+		private Instant moment;
+		private OrderStatus status;
 		
+		@ManyToOne
+		@JoinColumn(name = "client_id")
+		private User client;
 		
+		@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+		private Payment payment;
 		
-		public Order() {
+		@OneToMany(mappedBy = "id.order")
+		private Set<OrderItem> items = new HashSet<>();	
+		
 			
-			}
-
-		public Order(Long id, Instant moment, OrderStatus status, User client) {
-			
-			this.id = id;
-			this.moment = moment;
-			this.status = status;
-			this.client = client;
+	public Order() {
 		}
-
-
-
-
-		public Long getId() {
-			return id;
-		}
-
-
-
-		public void setId(Long id) {
-			this.id = id;
-		}
-
-
-
-		public Instant getMoment() {
-			return moment;
-		}
-
-
-
-		public void setMoment(Instant moment) {
-			this.moment = moment;
-		}
-
-
-
-		public OrderStatus getStatus() {
-			return status;
-		}
-
-
-
-		public void setStatus(OrderStatus status) {
-			this.status = status;
-		}
-
-
-
-		public User getClient() {
-			return client;
-		}
-
-
-
-		public void setClient(User client) {
-			this.client = client;
-		}
+	
+	public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
 		
+		this.id = id;
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+		this.payment = payment;
 		}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Instant moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public User getClient() {
+		return client;
+	}
+
+	public void setClient(User client) {
+		this.client = client;
+	}
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}	
+	
+	public Set<OrderItem> getItems() {
+		return items;
+	}
+	
+	public List<Product> getProducts() {
+		return items.stream().map(OrderItem::getProduct).collect(Collectors.toList());
+	}
+		
+	}
--- src/main/java/com/xicaum59/dscommerce/entities/OrderStatus.java
+++ src/main/java/com/xicaum59/dscommerce/entities/OrderStatus.java
@@ -1,7 +1,6 @@
 package com.xicaum59.dscommerce.entities;
 
 public enum OrderStatus {
-	
-	WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
 
+	WAYTING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED; 
 }
