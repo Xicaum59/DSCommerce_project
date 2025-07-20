30ef1706e7ca52c6f7e4d136cafe348107ee9d61
Xico Utrabo <xico.utrabo@gmail.com>
2025-07-20 10:59:05 -0300

    Equals hashcode

--- src/main/java/com/xicaum59/dscommerce/entities/Order.java
+++ src/main/java/com/xicaum59/dscommerce/entities/Order.java
@@ -3,6 +3,7 @@ package com.xicaum59.dscommerce.entities;
 import java.time.Instant;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -99,5 +100,23 @@ import jakarta.persistence.Table;
 	public List<Product> getProducts() {
 		return items.stream().map(OrderItem::getProduct).collect(Collectors.toList());
 	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+	
 		
 	}
