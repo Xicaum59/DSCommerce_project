8bfcb206709f75020b2b97ab269b7584d0f4c971
Xico Utrabo <xico.utrabo@gmail.com>
2025-09-16 22:40:31 -0300

    Get Products paged

--- src/main/java/com/xicaum59/dscommerce/services/ProductService.java
+++ src/main/java/com/xicaum59/dscommerce/services/ProductService.java
@@ -1,6 +1,9 @@
 package com.xicaum59.dscommerce.services;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,7 +25,12 @@ public class ProductService {
 		
 		
 		
-		
+		}
+	
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> result = repository.findAll(pageable);		
+		return result.map(x -> new ProductDTO(x));		
 	}
 
 }
