b07418211ac1ceb96422c02d52e8ab91d243efa9
Xico Utrabo <xico.utrabo@gmail.com>
2025-09-22 11:29:04 -0300

    DELETE Product

--- src/main/java/com/xicaum59/dscommerce/services/ProductService.java
+++ src/main/java/com/xicaum59/dscommerce/services/ProductService.java
@@ -52,6 +52,15 @@ public class ProductService {
 		return new ProductDTO(entity);
 		
 	}
+	
+	@Transactional
+	public void delete(Long id) {
+		repository.deleteById(id);		
+		
+		
+		
+		
+		}
 
 	private void copyDtoToEntity(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
